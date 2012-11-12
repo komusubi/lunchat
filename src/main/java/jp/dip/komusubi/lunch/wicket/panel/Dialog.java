3839556df88c5b242b7e6b09a11d53ab7f302a3a
jun ozeki <komusubi@gmail.com>
2012-11-12 16:42:52 +0900

    fix onCompoenntTag method because does not need it. (change wicket library ?)

--- src/main/java/jp/dip/komusubi/lunch/wicket/panel/Dialog.java
+++ src/main/java/jp/dip/komusubi/lunch/wicket/panel/Dialog.java
@@ -1,24 +1,23 @@
 /*
  * Licensed to the Apache Software Foundation (ASF) under one
- * or more contributor license agreements.  See the NOTICE file
+ * or more contributor license agreements. See the NOTICE file
  * distributed with this work for additional information
- * regarding copyright ownership.  The ASF licenses this file
+ * regarding copyright ownership. The ASF licenses this file
  * to you under the Apache License, Version 2.0 (the
  * "License"); you may not use this file except in compliance
- * with the License.  You may obtain a copy of the License at
+ * with the License. You may obtain a copy of the License at
  * 
- *   http://www.apache.org/licenses/LICENSE-2.0
+ * http://www.apache.org/licenses/LICENSE-2.0
  * 
  * Unless required by applicable law or agreed to in writing,
  * software distributed under the License is distributed on an
  * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
- * KIND, either express or implied.  See the License for the
+ * KIND, either express or implied. See the License for the
  * specific language governing permissions and limitations
- * under the License.    
+ * under the License.
  */
 package jp.dip.komusubi.lunch.wicket.panel;
 
-import org.apache.wicket.markup.ComponentTag;
 import org.apache.wicket.markup.html.basic.Label;
 import org.apache.wicket.markup.html.link.Link;
 import org.apache.wicket.markup.html.panel.FeedbackPanel;
@@ -26,106 +25,73 @@ import org.apache.wicket.markup.html.panel.Panel;
 import org.apache.wicket.model.IModel;
 
 /**
- * dialog.
+ * dialog component.
  * @author jun.ozeki
  * @since 2012/01/15
  */
 public abstract class Dialog<T> extends Panel {
-	
-	private static final long serialVersionUID = 6968078827891651580L;
-//	private static final Logger logger = LoggerFactory.getLogger(Dialog.class);
-	
-	public Dialog(String id, IModel<T> model) {
-		super(id, model);
-		add(getLabel("message"));
-		add(new FeedbackPanel("feedback"));
+
+    private static final long serialVersionUID = 6968078827891651580L;
+
+    /**
+     * create new instance.
+     * @param id wicket id
+     * @param model 
+     */
+    public Dialog(String id, IModel<T> model) {
+        super(id, model);
+        add(getLabel("message"));
+        add(new FeedbackPanel("feedback"));
 //		add(new DialogForm("confirm.form"));
-		add(getAgreeLink("agree"));
-		add(getCancelLink("cancel"));
-	}
+        add(getAgreeLink("agree"));
+        add(getCancelLink("cancel"));
+    }
 
-	protected Link<String> getAgreeLink(String id) {
-		return new Link<String>(id) {
+    /**
+     * create new agree event <code>Link</code>.
+     * @param id wicket id
+     * @return
+     */
+    protected Link<String> getAgreeLink(String id) {
+        return new Link<String>(id) {
 
-			private static final long serialVersionUID = -2374232038734113706L;
+            private static final long serialVersionUID = 1L;
 
-			@Override
-			public void onClick() {
-				onAgree();
-			}
-			
-			@Override
-			public void onComponentTag(ComponentTag tag) {
-				super.onComponentTag(tag);
-				// build fail url in jquery mobile dialog view. just fix.
-				tag.put("href", "wicket/" + getURL());
-			}
-		};
-	}
-	
-	protected Link<String> getCancelLink(String id) {
-		return new Link<String>(id) {
+            @Override
+            public void onClick() {
+                onAgree();
+            }
+        };
+    }
+    
+    /**
+     * create new cancel event <code>Link</code>.
+     * @param id wicket id
+     * @return
+     */
+    protected Link<String> getCancelLink(String id) {
+        return new Link<String>(id) {
 
-			private static final long serialVersionUID = 3048569339880231264L;
+            private static final long serialVersionUID = 1L;
 
-			@Override
-			public void onClick() {
-				onCancel();
-			}
-			
-			@Override
-			public void onComponentTag(ComponentTag tag) {
-			    super.onComponentTag(tag);
-			    tag.put("href", "wicket/" + getURL());
-			}
-		};
-	}
-	
-	/*
-	 * dialog form.
-	 * @author jun.ozeki
-	 * @since 2012/01/15
-	 */
-/*
-	public class DialogForm extends Form<Void> {
-		
-		private static final long serialVersionUID = -3788490402589716737L;
+            @Override
+            public void onClick() {
+                onCancel();
+            }
+        };
+    }
 
-		public DialogForm(String id) {
-			super(id);
-			add(getAgreeButton("agree"));
-			add(getCancelButton("cancel"));
-		}
-				
-		public Button getAgreeButton(String id) {
-			return new Button(id) {
-				
-				private static final long serialVersionUID = 4427926130247667179L;
+    /**
+     * action on event of agree.
+     */
+    protected abstract void onAgree();
 
-				@Override
-				public void onSubmit() {
-					onAgree();
-				}
-			};
-		}
-		
-		public Button getCancelButton(String id) {
-			return new Button(id) {
-				
-				private static final long serialVersionUID = 745625158114441901L;
+    /**
+     * action on event of cancel.
+     */
+    protected abstract void onCancel();
 
-				@Override
-				public void onSubmit() {
-					onCancel();
-				}
-			};
-		}
-	}
-*/	
-	protected abstract void onAgree();
-	protected abstract void onCancel();
-	
-	protected Label getLabel(String id) {
-	    return new Label(id, "ラベル");
-	}
+    protected Label getLabel(String id) {
+        return new Label(id, "ラベル");
+    }
 }
