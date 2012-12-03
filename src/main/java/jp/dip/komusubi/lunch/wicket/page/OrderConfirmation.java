55ac4359da66f5a873c914b551d445fbfe5d1d2c
jun ozeki <komusubi@gmail.com>
2012-12-03 15:34:47 +0900

    fix html hierarchy

--- src/main/java/jp/dip/komusubi/lunch/wicket/page/OrderConfirmation.java
+++ src/main/java/jp/dip/komusubi/lunch/wicket/page/OrderConfirmation.java
@@ -1,20 +1,20 @@
 /*
  * Licensed to the Apache Software Foundation (ASF) under one
- * or more contributor license agreements.  See the NOTICE file
+ * or more contributor license agreements. See the NOTICE file
  * distributed with this work for additional information
- * regarding copyright ownership.  The ASF licenses this file
+ * regarding copyright ownership. The ASF licenses this file
  * to you under the Apache License, Version 2.0 (the
  * "License"); you may not use this file except in compliance
- * with the License.  You may obtain a copy of the License at
+ * with the License. You may obtain a copy of the License at
  * 
- *   http://www.apache.org/licenses/LICENSE-2.0
+ * http://www.apache.org/licenses/LICENSE-2.0
  * 
  * Unless required by applicable law or agreed to in writing,
  * software distributed under the License is distributed on an
  * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
- * KIND, either express or implied.  See the License for the
+ * KIND, either express or implied. See the License for the
  * specific language governing permissions and limitations
- * under the License.    
+ * under the License.
  */
 package jp.dip.komusubi.lunch.wicket.page;
 
@@ -28,6 +28,7 @@ import jp.dip.komusubi.lunch.module.Basket;
 import jp.dip.komusubi.lunch.service.Shopping;
 import jp.dip.komusubi.lunch.wicket.WicketApplication;
 import jp.dip.komusubi.lunch.wicket.WicketSession;
+import jp.dip.komusubi.lunch.wicket.component.AuthorizedPage;
 import jp.dip.komusubi.lunch.wicket.component.FormKey;
 import jp.dip.komusubi.lunch.wicket.panel.Dialog;
 
@@ -37,70 +38,88 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 /**
- * confirm page. 
+ * confirm page for dialog.
+ * this page expect for 'date-rel' jquery attrbute is 'dialog'.
  * @author jun.ozeki
  */
-public class OrderConfirmation extends Confirmation {
+public class OrderConfirmation extends AuthorizedPage {
+    
+    private static final Logger logger = LoggerFactory.getLogger(OrderConfirmation.class);
+    private static final long serialVersionUID = 1L;
+    private FormKey key;
+    private Model<Product> model;
 
-	private static final Logger logger = LoggerFactory.getLogger(OrderConfirmation.class);
-	private static final long serialVersionUID = 6448297553843014369L;
-	private FormKey key;
+    /**
+     * create new instance.
+     * @param model
+     */
+    public OrderConfirmation(Model<Product> model) {
+        this.model = model;
+//        add(getDialog("confirm", model));
+    }
 
-	public OrderConfirmation(Model<Product> model) {
-		add(new Label("header.title", "タイトル"));
-		add(getDialog("confirm", model));
-	}
+    /**
+     * initialize components.
+     */
+    @Override
+    protected void onInitialize() {
+        super.onInitialize();
+        add(getDialog("confirm", model));
+        this.key = new FormKey(getPageId(), getId(), new Date());
+    }
 
-	@Override
-	protected void onInitialize() {
-		super.onInitialize();
-		this.key = new FormKey(getPageId(), getId(), new Date());
-	}
-	
-	@Override
-	protected void onConfigure() {
-		super.onConfigure();
-		WicketSession.get().addFormKey(key);
-	}
-	
-	protected Dialog<Product> getDialog(String id, final Model<Product> model) {
-		return new Dialog<Product>(id, model) {
+    /**
+     * configure components.
+     */
+    @Override
+    protected void onConfigure() {
+        super.onConfigure();
+        WicketSession.get().addFormKey(key);
+    }
 
-			private static final long serialVersionUID = 1351931925422304411L;
-			
-			@Override
-			protected void onAgree() {
-				try {
-					if (WicketSession.get().removeFormKey(key)) {
-					    // FIXME shopping with a basket !! 
-					    Shopping shopping = Configuration.getInstance(Shopping.class);
-					    Basket basket = shopping.getBasket(WicketSession.get().getSignedInUser());
-					    basket.add(model.getObject());
-					    shopping.order(basket);
-					    List<Order> orders = basket.getOrders();
-					    Order order = orders.get(0);
-					    setResponsePage(new OrderComplete(Model.of(order)));
-					} else {
-						// FIXME warning double submit.
-						error("double submit!!");
-						return;
-					}
-				} catch (Exception e) {
-					logger.warn("exception: {}", e);
-					error("order fail. " + model.getObject());
-				}
-			}
+    /**
+     * product dialog panel.
+     * @param id
+     * @param model
+     * @return
+     */
+    protected Dialog<Product> getDialog(String id, final Model<Product> model) {
+        return new Dialog<Product>(id, model) {
 
-			@Override
-			protected void onCancel() {
-			    setResponsePage(WicketApplication.get().getHomePage());
-			}
-			
-			@Override
-			protected Label getLabel(String id) {
-			    return new Label(id, model.getObject().getName() + " を注文します。");
-			}
-		};
-	}
+            private static final long serialVersionUID = 1L;
+
+            @Override
+            protected void onAgree() {
+                try {
+                    if (WicketSession.get().removeFormKey(key)) {
+                        // FIXME shopping with a basket !!
+                        Shopping shopping = Configuration.getInstance(Shopping.class);
+                        Basket basket = shopping.getBasket(WicketSession.get().getSignedInUser());
+                        basket.add(model.getObject());
+                        shopping.order(basket);
+                        List<Order> orders = basket.getOrders();
+                        Order order = orders.get(0);
+                        setResponsePage(new OrderComplete(Model.of(order)));
+                    } else {
+                        error(getString("double.submit"));
+                        return;
+                    }
+                } catch (Exception e) {
+                    logger.warn("exception: {}", e);
+                    error("order fail. " + model.getObject());
+                }
+            }
+
+            @Override
+            protected void onCancel() {
+                setResponsePage(WicketApplication.get().getHomePage());
+            }
+
+            @Override
+            protected Label getLabel(String id) {
+                return new Label(id, getString("order.to", model));
+            }
+        };
+    }
 
 }
