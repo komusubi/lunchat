2ae965062e1aa5830f699aeb30753cdf3a409531
jun ozeki <komusubi@gmail.com>
2012-12-03 15:27:56 +0900

    fix label component to wicket:message

--- src/main/java/jp/dip/komusubi/lunch/wicket/page/Login.java
+++ src/main/java/jp/dip/komusubi/lunch/wicket/page/Login.java
@@ -1,20 +1,20 @@
 /*
  * Licensed to the Apache Software Foundation (ASF) under one
- * or more contributor license agreements.  See the NOTICE file
+ * or more contributor license agreements. See the NOTICE file
  * distributed with this work for additional information
- * regarding copyright ownership.  The ASF licenses this file
+ * regarding copyright ownership. The ASF licenses this file
  * to you under the Apache License, Version 2.0 (the
  * "License"); you may not use this file except in compliance
- * with the License.  You may obtain a copy of the License at
+ * with the License. You may obtain a copy of the License at
  * 
- *   http://www.apache.org/licenses/LICENSE-2.0
+ * http://www.apache.org/licenses/LICENSE-2.0
  * 
  * Unless required by applicable law or agreed to in writing,
  * software distributed under the License is distributed on an
  * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
- * KIND, either express or implied.  See the License for the
+ * KIND, either express or implied. See the License for the
  * specific language governing permissions and limitations
- * under the License.    
+ * under the License.
  */
 package jp.dip.komusubi.lunch.wicket.page;
 
@@ -25,13 +25,11 @@ import jp.dip.komusubi.lunch.service.AccountService;
 import jp.dip.komusubi.lunch.util.Nonce;
 import jp.dip.komusubi.lunch.wicket.WicketSession;
 import jp.dip.komusubi.lunch.wicket.component.FormKey;
+import jp.dip.komusubi.lunch.wicket.component.ApplicationFrame;
 import jp.dip.komusubi.lunch.wicket.page.account.Registry;
 import jp.dip.komusubi.lunch.wicket.panel.EmailEntry;
-import jp.dip.komusubi.lunch.wicket.panel.Footer;
-import jp.dip.komusubi.lunch.wicket.panel.Header;
 import jp.dip.komusubi.lunch.wicket.panel.SignIn;
 
-import org.apache.wicket.model.Model;
 import org.apache.wicket.protocol.http.RequestUtils;
 import org.apache.wicket.request.mapper.parameter.PageParameters;
 import org.apache.wicket.util.string.StringValue;
@@ -43,56 +41,75 @@ import org.slf4j.LoggerFactory;
  * @author jun.ozeki
  * @since 2011/11/19
  */
-public class Login extends VariationBase {
+public class Login extends ApplicationFrame {
 
+    private static final long serialVersionUID = 1L;
     private static final Logger logger = LoggerFactory.getLogger(Login.class);
-	private static final long serialVersionUID = -5101224283988545642L;
     private FormKey key;
-	
-	public Login() {
-		this(new PageParameters());
-	}
-	
-	@Override
-	protected void onInitialize() {
-	    super.onInitialize();
+
+    /**
+     * create new instance. 
+     */
+    public Login() {
+        this(new PageParameters());
+    }
+
+    /**
+     * create new instance.
+     * @param params page parameters
+     */
+    public Login(PageParameters params) {
+        StringValue activateValue = params.get("activate");
+        // FIXME should not change component any condition!
+        SignIn signIn;
+        if (!activateValue.isEmpty())
+            signIn = new SignIn("signInPanel", activateValue.toString());
+        else
+            signIn = new SignIn("signInPanel");
+
+        add(signIn);
+        add(getEmailEntry("registry"));
+    }
+
+    /**
+     * initialize components.
+     */
+    @Override
+    protected void onInitialize() {
+        super.onInitialize();
         this.key = new FormKey(getPageId(), getId(), new Date());
-	}
-	
-	@Override
-	protected void onConfigure() {
-	    super.onConfigure();
+    }
+
+    /**
+     * configure components.
+     */
+    @Override
+    protected void onConfigure() {
+        super.onConfigure();
+        // this page is login page, so do NOT "login" component. 
+        get("signIn").setVisible(false);
+        get("signOut").setVisible(false);
         WicketSession.get().addFormKey(key);
-	}
-	
-	public Login(PageParameters params) {
-		StringValue activateValue = params.get("activate");
-		// FIXME should not change component any condition!
-		SignIn signIn;
-		if (!activateValue.isEmpty())
-			signIn = new SignIn("signInPanel", activateValue.toString());
-		else
-			signIn = new SignIn("signInPanel");
-		
-		add(signIn);
-		add(new Header("header", Model.of(getDefaultHeaderBean(getString("page.title"))), false));
-		add(getEmailEntry("registry")); 
-		add(new Footer("footer"));
-	}
-	
-	protected EmailEntry getEmailEntry(String id) {
-	    return new EmailEntry(id) {
-	        private static final long serialVersionUID = 3560368059107652338L;
+    }
+
+    /**
+     * 
+     * @param id
+     * @return
+     */
+    protected EmailEntry getEmailEntry(String id) {
+        return new EmailEntry(id) {
+            private static final long serialVersionUID = 3560368059107652338L;
 
             @Override
-	        protected void onRegister() {
+            protected void onRegister() {
                 if (WicketSession.get().removeFormKey(key)) {
                     // confirm page の absolute URLを取得
                     String targetPath = getRequestCycle().urlFor(Registry.class, null).toString();
                     String ownUrl = getRequestCycle().getUrlRenderer().renderFullUrl(getRequest().getClientUrl());
                     String url = RequestUtils.toAbsolutePath(ownUrl, targetPath);
-                    
-                    // already exist email ?  
+
+                    // already exist email ?
                     AccountService account = Configuration.getInstance(AccountService.class);
                     if (account.findByEmail(getUser().getEmail()) != null) {
                         error(getLocalizer().getString("already.exist.email", this, "email address already exist."));
@@ -105,7 +122,7 @@ public class Login extends VariationBase {
                 } else {
                     logger.info("double click on EmailEntry#onRegister");
                 }
-	        }
-	    };
-	}
+            }
+        };
+    }
 }
