be72f84bcc563c7e9e79b235e4b0e000994c51f3
jun ozeki <komusubi@gmail.com>
2012-12-08 00:42:23 +0900

    fix markup hierarchy.

--- src/main/java/jp/dip/komusubi/lunch/wicket/page/OrderComplete.java
+++ src/main/java/jp/dip/komusubi/lunch/wicket/page/OrderComplete.java
@@ -1,20 +1,20 @@
 /*
  * Licensed to the Apache Software Foundation (ASF) under one
- * or more contributor license agreements.  See the NOTICE file
+ * or more contributor license agreements. See the NOTICE file
  * distributed with this work for additional information
- * regarding copyright ownership.  The ASF licenses this file
+ * regarding copyright ownership. The ASF licenses this file
  * to you under the Apache License, Version 2.0 (the
  * "License"); you may not use this file except in compliance
- * with the License.  You may obtain a copy of the License at
+ * with the License. You may obtain a copy of the License at
  * 
- *   http://www.apache.org/licenses/LICENSE-2.0
+ * http://www.apache.org/licenses/LICENSE-2.0
  * 
  * Unless required by applicable law or agreed to in writing,
  * software distributed under the License is distributed on an
  * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
- * KIND, either express or implied.  See the License for the
+ * KIND, either express or implied. See the License for the
  * specific language governing permissions and limitations
- * under the License.    
+ * under the License.
  */
 package jp.dip.komusubi.lunch.wicket.page;
 
@@ -36,8 +36,9 @@ import org.slf4j.LoggerFactory;
  * @author jun.ozeki
  */
 public class OrderComplete extends AuthorizedFrame {
+
     private static final Logger logger = LoggerFactory.getLogger(OrderComplete.class);
-	private static final long serialVersionUID = -6096514197924442740L;
+    private static final long serialVersionUID = 1L;
     private FormKey key;
 
     /**
@@ -45,71 +46,70 @@ public class OrderComplete extends AuthorizedFrame {
      * @param model
      */
     public OrderComplete(IModel<Order> model) {
-	    add(getOrderLines("order.lines", model));
-	}
-    
+        add(getOrderLines("order.lines", model));
+    }
+
     /**
      * initialize components.
      */
     @Override
-	protected void onInitialize() {
-	    super.onInitialize();
+    protected void onInitialize() {
+        super.onInitialize();
         this.key = new FormKey(getPageId(), getId(), new Date());
-	}
-    
-	/**
-	 * configure components.
-	 */
-	@Override
-	protected void onConfigure() {
-	    super.onConfigure();
-	    WicketSession.get().addFormKey(key);
-	}
-	
-	/**
-	 * @param id  wicket id.
-	 * @param model Order. 
-	 * @return
-	 */
-	protected OrderLines getOrderLines(String id, IModel<Order> model) {
-	    return new UserOrderLines(id, model);
-	    /*
-	    return new UserOrderLines(id, model) {
-	        private static final long serialVersionUID = 7930054187191919478L;
+    }
 
-            @Override
-	        protected void onEat() {
-                if (WicketSession.get().removeFormKey(key)) {
-                    logger.info("いただきまーす");
-                } else {
-                    logger.info("double submit onEat()");
-                }
-	        }
+    /**
+     * configure components.
+     */
+    @Override
+    protected void onConfigure() {
+        super.onConfigure();
+        WicketSession.get().addFormKey(key);
+    }
 
-	        @Override
-	        protected void onFinish() {
-	            if (WicketSession.get().removeFormKey(key)) {
-	                logger.info("ごちそうさま");
-	            } else {
-	                logger.info("double submit onFinish()");
-	            }
-	        }
-	        
-//	        @Override
-	        protected void onCancel() {
-	            if (WicketSession.get().removeFormKey(key)) {
-	                logger.info("キャンセル");
-//	                AccountService account = Configuration.getInstance(AccountService.class);
-	                Shopping shopping = Configuration.getInstance(Shopping.class);
-	                Order order = ((IModel<Order>) getDefaultModel()).getObject(); 
-//	                shopping.cancel(order);
-	            } else {
-	                logger.info("double submit onCancel()");
-	            }
-	        }
-	    };
-	     */
-	}
-	
-	
+    /**
+     * @param id wicket id.
+     * @param model Order.
+     * @return
+     */
+    protected OrderLines getOrderLines(String id, IModel<Order> model) {
+        return new UserOrderLines(id, model);
+        /*
+         * return new UserOrderLines(id, model) {
+         * private static final long serialVersionUID = 7930054187191919478L;
+         * 
+         * @Override
+         * protected void onEat() {
+         * if (WicketSession.get().removeFormKey(key)) {
+         * logger.info("いただきまーす");
+         * } else {
+         * logger.info("double submit onEat()");
+         * }
+         * }
+         * 
+         * @Override
+         * protected void onFinish() {
+         * if (WicketSession.get().removeFormKey(key)) {
+         * logger.info("ごちそうさま");
+         * } else {
+         * logger.info("double submit onFinish()");
+         * }
+         * }
+         * 
+         * // @Override
+         * protected void onCancel() {
+         * if (WicketSession.get().removeFormKey(key)) {
+         * logger.info("キャンセル");
+         * // AccountService account = Configuration.getInstance(AccountService.class);
+         * Shopping shopping = Configuration.getInstance(Shopping.class);
+         * Order order = ((IModel<Order>) getDefaultModel()).getObject();
+         * // shopping.cancel(order);
+         * } else {
+         * logger.info("double submit onCancel()");
+         * }
+         * }
+         * };
+         */
+    }
+
 }
