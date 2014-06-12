c3222bd6aacd2fc12a9b58a08d2e3a2e5d3734e4
jun.ozeki <komusubi@gmail.com>
2014-06-12 15:33:23 +0900

    fix organize import

--- lunchat-web/src/main/java/jp/lunchat/web/service/Shopping.java
+++ lunchat-web/src/main/java/jp/lunchat/web/service/Shopping.java
@@ -32,6 +32,12 @@ import jp.lunchat.core.model.OrderLine.OrderLineKey;
 import jp.lunchat.core.model.Product;
 import jp.lunchat.core.model.Shop;
 import jp.lunchat.core.model.User;
+import jp.lunchat.storage.Basket;
+import jp.lunchat.storage.dao.OrderDao;
+import jp.lunchat.storage.dao.OrderLineDao;
+import jp.lunchat.storage.dao.ProductDao;
+import jp.lunchat.storage.dao.ShopDao;
+import jp.lunchat.storage.dao.TransactionException;
 
 import org.komusubi.common.util.Resolver;
 
